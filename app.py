1039ccc512f7f00ec3bfaf5a30531390cf38c452
flopo19 <florian.porta19@gmail.com>
2026-09-17 13:02:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,8 @@ def save_exercice(exo):
 col_logo, col_header = st.columns([1, 4])
 with col_logo:
     # Remplace l'URL ci-dessous par le lien vers le logo de ton club si tu en as un
-    st.image("images.png", width=80)    
+    st.image("images.jpg", width=80)
+    )
 with col_header:
     st.markdown(
         "<h1 class='main-title'>RUGBY COACH APP</h1>", unsafe_allow_html=True
